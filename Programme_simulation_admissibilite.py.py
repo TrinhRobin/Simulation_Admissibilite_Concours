2a5a598a7744fd03464bbee259f375ac410b71e4
Robin_ <77126392+TrinhRobin@users.noreply.github.com>
2023-10-21 19:27:16 +0200

    Update Programme_simulation_admissibilite.py.py

--- Programme_simulation_admissibilite.py.py
+++ Programme_simulation_admissibilite.py.py
@@ -1,43 +1,46 @@
-def moyenneCB():
-    MathEDHEC=float(input("note de Maths EDHEC?\n"))     
-    MathEML=float(input("note de Maths EM Lyon?\n"))      
-    MathEcricome=float(input("note de Maths Ecricome?\n"))       
-    MathII=float(input("note de Maths ESSEC II?\n"))       
-    PhiloHEC=float(input("note de philosophie HEC?\n"))       
-    PhiloEDHEC=float(input("note de philosophie EDHEC?\n"))       
-    PhiloEcricome=float(input("note de philosophie Ecricome?\n"))       
-    AnglaisELVi=float(input("note de anglais ELVi?\n"))       
-    AllemandELVi=float(input("note de Allemand ELVi?\n"))       
-    AnglaisEcricome=float(input("note de Anglais Ecricome?\n"))       
-    AllemandEcricome=float(input("note de Allemand Ecricome?\n"))       
-    Synthèse=float(input("note de synthèse?\n"))       
-    Résumé=float(input("note de résumé?\n"))       
-    GéopoESCP=float(input("note de HGG ESCP?\n"))       
-    GéopoESSEC=float(input("note de HGG ESSEC?\n"))       
-    GéopoEcricome=float(input("note de HGG Ecricome?\n"))
-       
+import numpy as np
 
-    y=(6*MathEDHEC+MathII*2+4*PhiloEDHEC+5*AnglaisELVi+2*AllemandELVi+3*Synthèse+8*GéopoESSEC)/30 #EDHEC       
-    c=(6*MathEML+MathII*2+6*PhiloHEC+4*AnglaisELVi+2*AllemandELVi+2*Synthèse+8*GéopoESCP)/30 #EMLYON       
-    b=(5*MathEML+5*PhiloHEC+6*AnglaisELVi+5*AllemandELVi+3*Synthèse+6*GéopoESCP)/30 #SKEMA       
-    a=(6*MathEcricome+5*PhiloEcricome+5*AnglaisEcricome+3*AllemandEcricome+2*Résumé+9*GéopoEcricome)/30 #NEOMA       
-    x=(9*MathEDHEC+6*PhiloHEC+4*AnglaisELVi+3*AllemandELVi+3*Synthèse+6*GéopoESCP)/30 #AUDENCIA
-        
-    print("Ta note à l'EDHEC :",y)       
-    print("Ta note à l'EM Lyon:",c)       
-    print("Ta note à SKEMA:",b)       
-    print("Ta note à NEOMA :",a)       
-    print("Ta note à Audencia :",x)
+def moyenneCB(noms_ecoles:list, seuils_admissibilite:list,coefficients:list)->None:
+    
+    notes = np.array([
+        float(input("note de Maths EDHEC?\n")),
+        float(input("note de Maths EM Lyon?\n")),
+        float(input("note de Maths Ecricome?\n")),
+        float(input("note de Maths ESSEC II?\n")),
+        float(input("note de philosophie HEC?\n")),
+        float(input("note de philosophie EDHEC?\n")),
+        float(input("note de philosophie Ecricome?\n")),
+        float(input("note de anglais ELVi?\n")),
+        float(input("note de Allemand ELVi?\n")),
+        float(input("note de Anglais Ecricome?\n")),
+        float(input("note de Allemand Ecricome?\n")),
+        float(input("note de synthèse?\n")),
+        float(input("note de résumé?\n")),
+        float(input("note de HGG ESCP?\n")),
+        float(input("note de HGG ESSEC?\n")),
+        float(input("note de HGG Ecricome?\n"))
+    ])
 
-    if y>12.63:
-        print("Admissible à EDHEC")       
-    if c>11.8:
-        print("Admissible à EM LYON")       
-    if b>10.8:
-        print("Admissible à Skema")
-    if a>10.4:
-        print("Admissible à NEOMA")     
-    if x>9.5:
-        print("Admissible à Audencia")
+    resultats = np.dot(coefficients, notes)/30
+
+    for i, resultat in enumerate(resultats):
+        print(f"Ta note à {noms_ecoles[i]}: {resultat:.2f}")
+
+        # Vérification des seuils d'admissibilité
+        if resultat > seuils_admissibilite[i]:
+            print(f"Admissible à {noms_ecoles[i]}")
+
+# Exemple d'utilisation de la fonction
 if __name__ == '__main__':
-    moyenneCB()
+    noms_ecoles = ["EDHEC", "EM LYON", "Skema", "NEOMA", "Audencia"]
+    seuils_admissibilite = [12.63, 11.8, 10.8, 10.4, 9.5]
+    coefficients = np.array([
+            [6, 0, 0, 2, 0, 4, 0, 5, 2, 0, 0, 3, 0, 0, 8,0],  # Coefficients pour l'EDHEC
+            [0, 6, 0, 2, 6, 0, 0, 4, 2, 0, 0, 2, 0, 8, 0, 0],  # Coefficients pour l'EMLYON
+            [0, 5, 0, 0, 6, 0, 0, 6, 5, 0, 0, 3, 0, 6, 0, 0],  # Coefficients pour SKEMA
+            [0, 0, 6, 0, 0, 0, 5, 0, 0, 5, 3, 0, 2, 0, 0, 9],  # Coefficients pour NEOMA
+            [9, 0, 0, 0, 6, 0, 0, 4, 3, 0, 0, 3, 0, 6, 0, 0]   # Coefficients pour Audencia
+        ])
+
+    moyenneCB(noms_ecoles, seuils_admissibilite,coefficients)
+
